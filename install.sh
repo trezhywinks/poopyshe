58452da8f1a81d6f2bee3d977f5caf52156e0df1
trezhywinks <dreqxyonion@gmail.com>
2025-05-13 18:09:36 +0000

    poopyshe shell push(1)

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo apt update
-sudo apt install npm -y
-sudo npm install .
+
+##sudo apt update
+##sudo apt install npm -y
+##sudo npm install .
 cp -r poopyshe cd /bin/
+cd shell_script && bash poopyshe.sh
