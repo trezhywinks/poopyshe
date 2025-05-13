b21040e619f4075ba68372d908255039a035efc8
trezhywinks <dreqxyonion@gmail.com>
2025-05-13 01:36:56 +0000

    poopyshe shell push(1)

---
+++ shell_script/poopyshe.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+LOADING_SHELL(){
+clear
+echo -e "[ poopyshe ] script Shell"
+echo -e "[ poopyshe ] loading."
+sleep 0.75
+clear
+echo -e "[ poopyshe ] script Shell"
+echo -e "[ poopyshe ] loading.."
+sleep 0.75
+clear
+echo -e "[ poopyshe ] script Shell"
+echo -e "[ poopyshe ] loading..."
+sleep 0.75
+clear
+BANNER_POOPY
+}
+
+BANNER_POOPY(){
+cat << EOF
+welcome to poopyshe
+try poopyshe --help to see the commands
+
+ ██▓███  ▒█████  ▒█████  ██▓███▓██   ██▓ ██████ ██░ ██▓█████ 
+▓██░  ██▒██▒  ██▒██▒  ██▓██░  ██▒██  ██▒██    ▒▓██░ ██▓█   ▀ 
+▓██░ ██▓▒██░  ██▒██░  ██▓██░ ██▓▒▒██ ██░ ▓██▄  ▒██▀▀██▒███   
+▒██▄█▓▒ ▒██   ██▒██   ██▒██▄█▓▒ ▒░ ▐██▓░ ▒   ██░▓█ ░██▒▓█  ▄ 
+▒██▒ ░  ░ ████▓▒░ ████▓▒▒██▒ ░  ░░ ██▒▓▒██████▒░▓█▒░██░▒████▒
+▒▓▒░ ░  ░ ▒░▒░▒░░ ▒░▒░▒░▒▓▒░ ░  ░ ██▒▒▒▒ ▒▓▒ ▒ ░▒ ░░▒░░░ ▒░ ░
+░▒ ░      ░ ▒ ▒░  ░ ▒ ▒░░▒ ░    ▓██ ░▒░░ ░▒  ░ ░▒ ░▒░ ░░ ░  ░
+░░      ░ ░ ░ ▒ ░ ░ ░ ▒ ░░      ▒ ▒ ░░ ░  ░  ░  ░  ░░ ░  ░   
+            ░ ░     ░ ░         ░ ░          ░  ░  ░  ░  ░  ░
+                                ░ ░                          
+EOF
+}
+
+LOADING_SHELL
